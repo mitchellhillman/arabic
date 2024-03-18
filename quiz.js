18b7576b461777742e204c9d72e44a31e53a638f
Mitchell Hillman <mitchellhillman@gmail.com>
2024-03-18 11:58:05 -0400

    refactor quiz

--- quiz.js
+++ quiz.js
@@ -10,7 +10,7 @@ const rl = readline.createInterface({
 
 let phrases = [];
 
-const quiz = async () => {
+const quizArabicEng = async () => {
   for (let step = 0; step < phrases.length; step++) {
     const count = `${step + 1}/${phrases.length}: `;
     const tab = count
@@ -27,11 +27,29 @@ const quiz = async () => {
   rl.close();
 };
 
+const quizEngArabic = async () => {
+  for (let step = 0; step < phrases.length; step++) {
+    const count = `${step + 1}/${phrases.length}: `;
+    const tab = count
+      .split("")
+      .map(() => " ")
+      .join("");
+    await new Promise((resolve) =>
+      rl.question(`${count}${phrases[step][1].trim()} `, resolve)
+    );
+    console.log(`${tab}=> ${phrases[step][0].trim()}\n`);
+  }
+
+  console.log("Complete!");
+  rl.close();
+};
+
+
 fs.createReadStream("./vocab.csv")
   .pipe(parse({ delimiter: "," }))
   .on("data", (row) => {
     phrases.push(row);
   })
   .on("end", () => {
-    quiz(shuffle(phrases));
+    quizEngArabic(shuffle(phrases));
   });
